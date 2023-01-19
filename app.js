ba5dd3cd6a88b1500d86124df30c6eb7b58570bd
Aditya <as23july@gmail.com>
2023-01-20 00:58:01 +0530

    new

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const request = require('request')
-const url = 'https://api.open-meteo.com/v1/forecast?latitude=28.71&longitude=77.22'
+const url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/delhi?unitGroup=metric&key=4CJKKG4BXPU5ZX7QHJHKX3RD7&contentType=json'
 
 request({ url: url, json: true}, (error, response) => {
-    console.log(response.body)
+    console.log(response.body.currentConditions)
 })
